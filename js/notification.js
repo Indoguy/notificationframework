812d0b11dca10bd6c06f31fef50a9ba58443f6b0
Unknown <ties_aarts@outlook.com>
2017-06-21 17:46:41 +0200

    Update Feedback
    Feedback Luuk

--- js/notification.js
+++ js/notification.js
@@ -1,28 +1,26 @@
 //--------------------------------------------------------------------------//
-//----------------------Permission request, on page load--------------------//
-//--------------------------------------------------------------------------//
-document.addEventListener('DOMContentLoaded', function () {
-  if (!Notification) {
-    alert('Desktop notifications not available in your browser. Try Chromium.'); 
-    return;
-  }
-
-  if (Notification.permission !== "granted")
-    Notification.requestPermission();
-});
-
-//--------------------------------------------------------------------------//
-//-----------------------------Framework Code-------------------------------//
+//-------------------------------Library Code-------------------------------//
 //--------------------------------------------------------------------------//
 function PopupNotification(title, body, icon, link, delay) {
-	this.title = title;
-	this.body = body;
-	this.icon = icon;
-	this.link = link;
-	this.delay = delay;
+	this.title = title || 'Default title';
+	this.body = body || 'Default Message';
+	this.icon = icon || 'images/default.png';
+	this.link = link || 'https://github.com/Indoguy/notificationframework';
+	this.delay = delay || 1000;
 	
 	// The normal popup
 	this.popup = function () {
+		// Permission request, on page load
+		document.addEventListener('DOMContentLoaded', function () {
+		  if (!Notification) {
+			alert('Desktop notifications not available in your browser. Try Chromium.'); 
+			return;
+		  }
+
+		  if (Notification.permission !== "granted")
+			Notification.requestPermission();
+		});
+		
 		if (Notification.permission !== "granted") Notification.requestPermission();
 		else {
 			var notification = new Notification(title, {
